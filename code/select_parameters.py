85ef2761403079c11f7d2427879724ffc10bcd61
Wyman <54023029+1511pspsps@users.noreply.github.com>
2020-10-07 21:14:22 +0800

    Use Grid Search to find parameters

--- code/select_parameters.py
+++ code/select_parameters.py
@@ -8,23 +8,24 @@ import numpy as np
 from sklearn.neural_network import MLPClassifier
 
 # Import the data
+
+# You should check the path before run the code
 data = pd.read_table('F:/各个学科/工业大数据/lab/project1/data/new_data.csv', sep=',')
 train = np.array(data)
 data = pd.read_table('F:/各个学科/工业大数据/lab/project1/data/test_set.csv', sep=',')
 test = np.array(data)
 
-# The fifth column is the label
 a = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 X_train = train[:, a]
 y_train = train[:, 4]
 X_test = test[:, a]
 y_test = test[:, 4]
 
-DT_clf__tuned_parameters = {"n_estimators": [10, 11, 12, 13, 14],
+RF_clf__tuned_parameters = {"n_estimators": [10, 11, 12, 13, 14],
                             "max_depth": [6, 7, 8, 9, 10, 11, 12, 13],
                             "min_samples_split": [2, 3, 4, 5, 6, 7]}
-DT = RandomForestClassifier()
-estimator = GridSearchCV(DT, DT_clf__tuned_parameters, n_jobs=5)
+RF = RandomForestClassifier()
+estimator = GridSearchCV(RF, RF_clf__tuned_parameters, n_jobs=5)
 estimator.fit(X_train, y_train)
 print("---------------------------------------------------------")
 print(estimator.best_params_)
