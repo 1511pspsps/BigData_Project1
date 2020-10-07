df0e58cf90752497e5c024e21bf7bcd13aa9c14e
Wyman <54023029+1511pspsps@users.noreply.github.com>
2020-10-07 21:07:22 +0800

    Decision Tree Classifier

--- code/DT.py
+++ code/DT.py
@@ -2,12 +2,12 @@ import time
 import pandas as pd
 from sklearn.metrics import accuracy_score
 from sklearn.tree import DecisionTreeClassifier
-'''
+
 from sklearn.tree import export_graphviz
 from six import StringIO
 import pydotplus
 import os
-'''
+
 
 
 # Import the data
@@ -15,8 +15,13 @@ col_names = ['gameId', 'creationTime', 'gameDuration', 'seasonId', 'winner',
              'firstBlood', 'firstTower', 'firstInhibitor', 'firstBaron', 'firstDragon', 'firstRiftHerald',
              't1_towerKills', 't1_inhibitorKills', 't1_baronKills', 't1_dragonKills', 't1_riftHeraldKills',
              't2_towerKills', 't2_inhibitorKills', 't2_baronKills', 't2_dragonKills', 't2_riftHeraldKills']
+
+
+# You should check the path before run the code
 data = pd.read_csv("F:/各个学科/工业大数据/lab/project1/data/new_data.csv", header=None, names=col_names)
 test = pd.read_csv("F:/各个学科/工业大数据/lab/project1/data/test_set.csv", header=None, names=col_names)
+
+
 data = data.iloc[1:]
 test = test.iloc[1:]
 feature_cols = ['firstBlood', 'firstTower', 'firstInhibitor', 'firstBaron', 'firstDragon', 'firstRiftHerald',
@@ -40,7 +45,7 @@ DT_clf = DT_clf.fit(X_train, y_train)
 # Apply the tree
 y_pred = DT_clf.predict(X_test)
 print("Accuracy:", accuracy_score(y_test, y_pred))
-'''
+
 # Visualize the decision tree
 os.environ["PATH"] += os.pathsep + 'C:/Program Files (x86)/Graphviz2.38/bin/'
 dot_data = StringIO()
@@ -48,11 +53,3 @@ export_graphviz(DT_clf, out_file=dot_data, filled=True, rounded=True, special_ch
                 class_names=['0', '1'])
 graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
 graph.write_png('tree.png')
-
-
-
-'''
-
-
-
-
